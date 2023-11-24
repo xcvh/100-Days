49e19bdb9ca984eeade739f2fa8f90aea640a6a3
jel.do <101758793+xcvh@users.noreply.github.com>
2023-11-24 23:15:38 +0100

    More exploration, i feel dumb.

--- Memoization.playground/Contents.swift
+++ Memoization.playground/Contents.swift
@@ -1,8 +1,6 @@
 import Cocoa
 
-//let dict = [3: 1]
-//dict[3]
-//dict[2, default: 1]
+// MARK: Recursive Fib
 
 func fib(_ n: Int) -> Int {
     var result: Int
@@ -13,6 +11,8 @@ func fib(_ n: Int) -> Int {
     }
     return result
 }
+
+// MARK: Recursive fib with memoization
 
 var results = [Int:Int]()
 
@@ -27,8 +27,25 @@ func memoizedFib(_ n: Int) -> Int {
         result = memoizedFib(n-1) + memoizedFib(n-2)
     }
     results[n] = result
-//    print(results)
     return result
+}
+
+// MARK: Looped fib with memoization
+
+func loopedFib(_ n: Int) -> Int {
+    var memo = [Int:Int]()
+    var result: Int = 0
+    
+    for i in 1...n+1 {
+        if i <= 2 {
+            result = 1
+        } else {
+            result = memo[i-1]! + memo[i-2]!
+        }
+        memo[i] = result
+    }
+
+    return memo[n]!
 }
 
 let clock = ContinuousClock()
@@ -42,3 +59,15 @@ let memoElapsed = clock.measure {
     print(memoizedFib(i))
 }
 print("memoizedFib: \(memoElapsed)")
+
+let memoLooped = clock.measure {
+    print(loopedFib(i))
+}
+print("loopedFib: \(memoLooped)")
+
+//75025
+//fib: 8.159130583000001 seconds
+//75025
+//memoizedFib: 0.002710917 seconds
+//75025
+//loopedFib: 0.001554541 seconds
