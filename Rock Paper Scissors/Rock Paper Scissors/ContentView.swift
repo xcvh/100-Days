fc4d1a4b3b8136a7b3c64e5c623f53729292bb47
jel.do <101758793+xcvh@users.noreply.github.com>
2024-02-17 12:43:50 +0100

    I have no idea, since I fell off the wagon. Let's see if I can get back on track.

---
+++ Rock Paper Scissors/Rock Paper Scissors/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissors
+//
+//  Created by Jeldo Arno Meppen on 13.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let move = ["🪨", "📜", "✂️"]
+    @State private var currentMoveComputer = "🪨"
+    @State private var playerShouldWin = true
+    @State private var score = 0
+    @State private var round = 0
+    @State private var alertTitle = ""
+    @State private var scoreIsPresented = false
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+                Text(currentMoveComputer)
+                    .font(.largeTitle)
+                Text(playerShouldWin ? "Try to win" : "Try to lose")
+                Spacer()
+                HStack {
+                    ForEach(0..<3) {
+                        
+                        Button(move[$0]) {
+                            scoreIsPresented = true
+                        }
+                        .font(.system(size: 70))
+                        .padding()
+                    }
+                }
+                .alert(alertTitle, isPresented: $scoreIsPresented) {
+                    Button("OK") { playRound() }
+                }
+                Spacer()
+                HStack {
+                    Text("Score: \(score)")
+                    Spacer()
+                    Text("Round: \(round)")
+                }
+            }
+            .padding()
+        }
+    }
+    
+    func playRound() {
+        currentMoveComputer = move[Int.random(in: 0...2)]
+        playerShouldWin = Bool.random()
+        score += 1
+    }
+    
+    func testMove(computer: String, player: String, shouldWin: Bool) -> Bool {
+        let move = (computer, player) // ["🪨", "📜", "✂️"]
+        if shouldWin == true {
+            switch move {
+            case ("🪨", "📜"):
+                return true
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rock Paper Scissors/Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Jeldo Arno Meppen on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
